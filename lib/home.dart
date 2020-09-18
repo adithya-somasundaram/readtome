a39d289516da6c0bfc70ded5bf718dc76ddea4b9
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-09-18 13:15:13 -0700

    added future builder to read and load from passages json

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './entry.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -20,64 +13,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _loadEntries() async {
+    return await rootBundle.loadString("assets/passages.json");
+  }
+
+  Future _storeEntries() async {
+    String result = await _loadEntries();
+    return jsonDecode(result);
+  }
+
+  _helper(name, msg) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => Entry(title: name, message: msg)));
+      },
+      child: new Card(
+        child: ListTile(
+          title: Text(name),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    String temp = "The researchers found that word recall was greatest when the participants read aloud to themselves.\n\n“This study confirms that learning and memory benefit from active involvement,” says study author Colin M. MacLeod, a professor and chair of the Department of Psychology at the University of Waterloo.";
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            GestureDetector(
-              onTap: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => Entry(title: 'Entry 1', message: temp)
-                  )
-                );
-              },
-              child: new Card(
-                child: ListTile(
-                  title: Text('Entry 1'),
-                ),
-              ) ,
-            )
-          ],
-        ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: FutureBuilder(
+              future: _storeEntries(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                List<Widget> result;
+                if (snapshot.hasData) {
+                  result = <Widget>[
+                    for (var x in snapshot.data['entries'])
+                      _helper(x['name'], x['passage'])
+                  ];
+                } else if (snapshot.hasError) {
+                  result = <Widget>[
+                    Icon(
+                      Icons.check_circle_outline,
+                      color: Colors.green,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Result: ${snapshot.data}'),
+                    )
+                  ];
+                } else {
+                  result = <Widget>[
+                    Icon(
+                      Icons.check_circle_outline,
+                      color: Colors.green,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Result: ${snapshot.data}'),
+                    )
+                  ];
+                }
+                return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: result);
+              })),
     );
   }
 }
